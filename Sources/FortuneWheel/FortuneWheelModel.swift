6b4ad52e21666a066f35b7f6841c1a9aa15ddc05
Sameer <sameer.nwaz@gmail.com>
2022-10-26 10:58:52 -0700

    Update FW model access modifier

--- Sources/FortuneWheel/FortuneWheelModel.swift
+++ Sources/FortuneWheel/FortuneWheelModel.swift
@@ -22,12 +22,12 @@ public struct FortuneWheelModel {
     let animation: Animation
     let getWheelItemIndex: (() -> (Int))?
 
-    init(
+    public init(
         titles: [String], size: CGFloat, onSpinEnd: ((Int) -> ())?,
-        colors: [Color] = Color.spin_wheel_color,
-        pointerColor: Color = Color(hex: "DA4533"),
+        colors: [Color]? = nil,
+        pointerColor: Color? = nil,
         strokeWidth: CGFloat = 15,
-        strokeColor: Color = Color(hex: "252D4F"),
+        strokeColor: Color? = nil,
         animDuration: Double = Double(6),
         animation: Animation? = nil,
         getWheelItemIndex: (() -> (Int))? = nil
@@ -35,10 +35,10 @@ public struct FortuneWheelModel {
         self.titles = titles;
         self.size = size;
         self.onSpinEnd = onSpinEnd;
-        self.colors = colors;
-        self.pointerColor = pointerColor;
+        self.colors = colors ?? Color.spin_wheel_color;
+        self.pointerColor = pointerColor ?? Color(hex: "DA4533");
         self.strokeWidth = strokeWidth;
-        self.strokeColor = strokeColor;
+        self.strokeColor = strokeColor ?? Color(hex: "252D4F");
         self.animDuration = animDuration;
         self.animation = animation ?? Animation.timingCurve(0.51, 0.97, 0.56, 0.99, duration: animDuration);
         self.getWheelItemIndex = getWheelItemIndex;
